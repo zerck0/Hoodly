8a8592672e5d5f615815a6a0eca62fcf95a605ab
Tom Georgin <tom@georgin.net>
2026-05-14 16:44:51 +0200

    #121 feat(desktop): ajout de la dépendance sqlite-jdbc et configuration du module java

--- hoodly-desktop/src/main/java/module-info.java
+++ hoodly-desktop/src/main/java/module-info.java
@@ -4,12 +4,16 @@ module com.hoodly.hoodlydesktop {
     requires okhttp3;
     requires com.fasterxml.jackson.databind;
     requires java.desktop;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
 
     opens com.hoodly.hoodlydesktop to javafx.fxml;
     opens com.hoodly.hoodlydesktop.controllers to javafx.fxml;
     opens com.hoodly.hoodlydesktop.auth to javafx.fxml;
     opens com.hoodly.hoodlydesktop.models to javafx.fxml, javafx.base, com.fasterxml.jackson.databind;
     opens com.hoodly.hoodlydesktop.services to javafx.fxml;
+    opens com.hoodly.hoodlydesktop.db to javafx.fxml;
 
     exports com.hoodly.hoodlydesktop;
+    exports com.hoodly.hoodlydesktop.db;
 }
